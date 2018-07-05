d35fe6fc83e27d1a1bc97d0481e6613e805aeee9
Nick <nicksloss@me.com>
2018-07-05 03:47:46 -0400

    remove token for safety

--- bot.py
+++ bot.py
@@ -24,4 +24,4 @@ async def on_message(message):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
-client.run('NDU5NDMyODU0ODIxMTQyNTI5.Dg2Htw.aVPJBVaOHBEU8SjskX4plWhYiKg')
+client.run('')
